50cbcc0d5bb38134b7779744f7621bde823396aa
ss-jay <102974611+ss-jay@users.noreply.github.com>
2022-04-08 11:28:20 +0530

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,29 @@
 (function () {
     setTimeout(() => {
-        loadPageContent("homepage")
+        console.log("This is custom.js file");
+        window.addEventListener('message', function (eventData) {
+          console.log("CHECKING FOR EVENT : SHIV");
+          console.log(eventData);
+          try {
+              console.log("1 => ", eventData);
+              console.log("1.1 => ", JSON.parse(eventData.data));
+
+              if (JSON.parse(eventData.data)) {
+                  let event = JSON.parse(eventData.data);
+                  console.log("1.2", event)
+                  loadPageContent("homepage")
+                  console.log("AFTER :: page is loading');
+              }
+
+          } catch (error) {
+              return;
+          }
+        }, false);
     }, 500);
 })();
 
 function loadPageContent(page) {
+    console.log("page is loading');
     if(page === "homepage") {
         insertSearchBar();
         insertTabContainer();
