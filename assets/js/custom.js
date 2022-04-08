10bebe2a773cfb64a4ce9a3038c08b116f3e47c8
ss-jay <102974611+ss-jay@users.noreply.github.com>
2022-04-08 11:51:15 +0530

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,24 +1,25 @@
 (function () {
     setTimeout(() => {
-        console.log("This is custom.js file");
-        window.addEventListener('message', function (eventData) {
-          console.log("CHECKING FOR EVENT : Custom");
-          console.log(eventData);
-          try {
-              console.log("1 => ", eventData);
-              console.log("1.1 => ", JSON.parse(eventData.data));
-
-              if (JSON.parse(eventData.data)) {
-                  let event = JSON.parse(eventData.data);
-                  console.log("1.2", event)
                   loadPageContent("homepage")
-                  console.log("AFTER :: page is loading");
-              }
+        
+//         console.log("This is custom.js file");
+//         window.addEventListener('message', function (eventData) {
+//           console.log("CHECKING FOR EVENT : Custom");
+//           console.log(eventData);
+//           try {
+//               console.log("1 => ", eventData);
+//               console.log("1.1 => ", JSON.parse(eventData.data));
 
-          } catch (error) {
-              return;
-          }
-        }, false);
+//               if (JSON.parse(eventData.data)) {
+//                   let event = JSON.parse(eventData.data);
+//                   console.log("1.2", event)
+//                   console.log("AFTER :: page is loading");
+//               }
+
+//           } catch (error) {
+//               return;
+//           }
+//         }, false);
     }, 500);
 })();
 
